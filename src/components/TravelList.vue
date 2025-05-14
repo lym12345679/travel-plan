0dbcaa0b81790ca7de9be3a22e6997d7aae90025
lym12345679 <1457545136@qq.com>
2025-05-14 22:13:32 +0800

    fit: 拖拽功能

--- src/components/TravelList.vue
+++ src/components/TravelList.vue
@@ -1,5 +1,8 @@
 ﻿<script setup lang="ts">
 import { ref } from 'vue'
+import IconHotel from '@/components/icons/IconHotel.vue'
+import IconSpot from '@/components/icons/IconSpot.vue'
+import IconTransportation from '@/components/icons/IconTransportation.vue'
 
 const props = defineProps({
     id:Number,
@@ -12,6 +15,15 @@ const props = defineProps({
 
 <template>
   <div class="info-box" :data-i-id="props.id">
+    <span v-if="props.type === 'hotel'" class="icon">
+      <IconHotel />
+    </span>
+    <span v-else-if="props.type === 'spot'" class="icon">
+      <IconSpot />
+    </span>
+    <span v-else-if="props.type === 'transportation'" class="icon">
+      <IconTransportation />
+    </span>
     <p>Name: {{ props.name }}</p>
     <p>Type: {{ props.type }}</p>
     <p>Location: {{ props.location }}</p>
@@ -24,5 +36,10 @@ const props = defineProps({
   border: 1px solid #ccc; /* Adds a light gray border */
   padding: 5px; /* Adds some padding inside the box */
   border-radius: 5px; /* Optional: Rounds the corners */
+}
+
+.icon{
+  width: 50px;
+  height: 50px;
 }
 </style>
